7dceb17b8bf7f602e2b18ff43a7d3180e6c36275
emmiekaobishops <112969497+emmiekaobishops@users.noreply.github.com>
2023-11-24 22:01:32 -1000

    css not working

--- AndersonMcGortyLabDB_Test/settings.py
+++ AndersonMcGortyLabDB_Test/settings.py
@@ -165,6 +165,11 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'website/static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index),
     path("<int:aID>", views.materialInfo),
     path("<str:sort>", views.indexWithSort)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
